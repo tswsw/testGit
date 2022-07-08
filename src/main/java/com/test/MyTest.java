d08db4d773ffa5486e62a4a0f1591f55385589d3
毛彦栋 <mao_yd@hisuntech.com>
2022-07-08 11:15:08 +0800

    develop分支本地提交03

--- src/main/java/com/test/MyTest.java
+++ src/main/java/com/test/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("第一次本地修改，并提交");
         System.out.println("第二次本地修改，并提交");
+        System.out.println("develop分支第三次本地修改，并提交");
     }
 }
