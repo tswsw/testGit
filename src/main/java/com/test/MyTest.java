732a305d94f141b7b82ed39933971df75b32e67a
毛彦栋 <mao_yd@hisuntech.com>
2022-07-08 11:07:51 +0800

    本地提交02

--- src/main/java/com/test/MyTest.java
+++ src/main/java/com/test/MyTest.java
@@ -3,5 +3,6 @@ package com.test;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("第一次本地修改，并提交");
+        System.out.println("第二次本地修改，并提交");
     }
 }
